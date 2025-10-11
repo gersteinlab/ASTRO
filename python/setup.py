68fd3a2a013a5acda044b667739f3d046e1b397f
Yuhang Chen <54288763+yuhangchen0802@users.noreply.github.com>
2025-10-11 16:04:44 -0400

    feat(container): micromamba+bioconda Dockerfile for v1.0; update README; cleanup

--- python/setup.py
+++ python/setup.py
@@ -2,7 +2,7 @@ from setuptools import setup, find_packages
 
 setup(
     name="ASTRO",
-    version="0.2",
+    version="1.0",
     packages=find_packages(),
     entry_points={
         "console_scripts": [
